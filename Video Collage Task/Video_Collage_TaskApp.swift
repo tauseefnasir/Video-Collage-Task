fc37e4307171fbcc147321eed474c86d0e0fe657
tauseefnasir <tauseefnasir501@gmail.com>
2024-11-06 21:21:49 +0500

    Initial Commit

---
+++ Video Collage Task/Video_Collage_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Video_Collage_TaskApp.swift
+//  Video Collage Task
+//
+//  Created by Nasir on 06/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Video_Collage_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
